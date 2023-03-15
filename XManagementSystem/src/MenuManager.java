7191e846670aa9e1ec038948f6ac68a688376b8c
LeeGyeonghyeon <diturea@gmail.com>
2023-03-15 23:47:54 +0900

    added MenuManager.java delete hello.java

---
+++ XManagementSystem/src/MenuManager.java
@@ -0,0 +1,10 @@
+
+public class MenuManager {
+	public static void main(String[] args) {
+		System.out.println("1. Add Schedules");
+		System.out.println("2. Delete Schedules");
+		System.out.println("3. Edit Schedules");
+		System.out.println("4. View Schedules");
+		System.out.println("5. Exit");
+	}
+}
