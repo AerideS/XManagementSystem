a2a988fef97e103164d3f59624edb3056e586278
LeeGyeonghyeon <diturea@gmail.com>
2023-03-24 23:12:56 +0900

    Assignment2 eidted

--- XManagementSystem/src/MenuManager.java
+++ XManagementSystem/src/MenuManager.java
@@ -5,11 +5,10 @@ public class MenuManager {
 		Scanner input = new Scanner(System.in);
 		int select;
 		
-		int scheduleId;
-		String name;
-		int time;
-		int day;
-		
+		int scheduleId;		//Schedule id to distinguish each schedules
+		String name;		//Schedule name
+		int day;			//Schedule day
+int time;			//Schedule time
 		while(true) {
 			System.out.println("1. Add Schedules");
 			System.out.println("2. Delete Schedules");
@@ -22,19 +21,27 @@ public class MenuManager {
 			
 			switch(select) {
 			case 1:
+				System.out.println("input schedule id");
 				scheduleId = input.nextInt();
+				System.out.println("input name");
 				name = input.next();
+				System.out.println("input day");
 				day = input.nextInt();
+				System.out.println("input time");
 				time = input.nextInt();
 				break;
 			case 2:
 			case 3:
 			case 4:
 				System.out.println("What is the scheduleId?");
+				scheduleId = input.nextInt();
 				break;
+			case 5:
+				continue;
 			case 6:
 				return;
 			}
 		}
 	}
 }
+
