3db6cf78438aaef87f5d9334df32c856e231ea0c
LeeGyeonghyeon <diturea@gmail.com>
2023-03-29 21:27:18 +0900

    Assignment #3

--- XManagementSystem/src/MenuManager.java
+++ XManagementSystem/src/MenuManager.java
@@ -14,20 +14,56 @@ public class MenuManager {
 			System.out.println("6. Exit");
 			System.out.println("Select one number between 1-6:");
 			num = input.nextInt();
-			switch(num) {
-			case 1:
-				System.out.print("Schedule order:");
-				int scheduleOrder = input.nextInt();
-				System.out.print("Schedule name:");
-				String schduleName = input.next();
-				break;
-			case 2:
-			case 3:
-			case 4:
-				System.out.print("Schedule order:");
-				int scheduleOrder2 = input.nextInt();
-			}
+			
+			if(num == 1)
+				addSchedule();
+			else if(num == 2)
+				deleteSchedule();
+			else if(num == 3)
+				editSchedule();
+			else if(num ==4)
+				viewSchedule();
+			else 
+				continue;
+
 		}
+		
+
+	}
+
+	public static void addSchedule() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Schedule order:");
+		int scheduleOrder = input.nextInt();
+		
+		System.out.print("Schedule name:");
+		String scheduleName = input.next();
+
+		System.out.print("Subject:");
+		String scheduleSubject = input.next();
+ 
+		System.out.print("Dead Line:");
+		String deadLine = input.next();
+	 
+	}
+	
+	public static void deleteSchedule() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Schedule order");
+		int scheduleOrder = input.nextInt();
+	}
+	
+	public static void editSchedule() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Schedule order");
+		int scheduleOrder = input.nextInt();
+	}
+	
+	public static void viewSchedule() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Schedule order");
+		int scheduleOrder = input.nextInt();
 	}
 }
 
